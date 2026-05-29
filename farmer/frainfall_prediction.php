8532f7adc0d108ad77e86c5ac5248df1c53819fe
Sandeepa Oraon <sandeepa2004oraon@gmail.com>
2026-05-29 21:26:02 +0530

    rainfall changed

--- farmer/frainfall_prediction.php
+++ farmer/frainfall_prediction.php
@@ -14,104 +14,118 @@ $query4 = "SELECT * from farmerlogin where email='$user_check'";
 
 <!DOCTYPE html>
 <html>
+
+<head>
+    <style>
+
+    </style>
+    <link rel="stylesheet" href="../assets/css/card.css">
+</head>
 <?php include ('fheader.php');  ?>
 
-  <body class="bg-white" id="top">
-  
-<?php include ('fnav.php');  ?>
+<body class="bg-white" id="top">
 
- 
-  <section class="section section-shaped section-lg">
-    <div class="shape ">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-    </div>
-<!-- ======================================================================================================================================== -->
+    <?php include ('fnav.php');  ?>
 
-<div class="container ">
-    
-    	 <div class="row">
-          <div class="col-md-8 mx-auto text-center">
-            <span class="badge badge-danger badge-pill mb-3">Prediction</span>
-          </div>
+
+    <section class="section section-shaped section-lg">
+        <div class="shape ">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
         </div>
-		
-          <div class="row row-content">
-            <div class="col-md-12 mb-3">
+        <!-- ======================================================================================================================================== -->
 
-				<div class="card text-white bg-gradient-success mb-3">
-				<form role="form" action="#" method="post" >  
-				  <div class="card-header">
-				  <span class=" text-info display-4" > Rainfall Prediction  </span>	
-				  
-				  </div>
+        <div class="container ">
 
-				  <div class="card-body text-dark">
-					 
-				<table class="table table-striped table-hover table-bordered bg-gradient-white text-center display" id="myTable">
+            <div class="row row-content">
+                <div class="col-md-12 mb-3">
 
-    <thead>
-					<tr class="font-weight-bold text-default">
-					<th><center>Region</center></th>
-					<th><center>Month</center></th>
-					<th><center>Prediction</center></th>
-					
-					
-        </tr>
-    </thead>
- <tbody>
-                                 <tr class="text-center">
+                    <div class="text-white  mb-3">
+                        <form role="form" action="#" method="post">
+                            <div class="f-login">
+                                <p>Rainfall Prediction</p>
+                            </div>
 
-                                   <td>
-                                    	<div class="form-group ">
-											    <select id="region-select" name="region" class="form-control" required>
-													<option value="">Select Region</option>
-												</select>
-												<script language="javascript"> print_region("region-select"); </script>
-										</div>
-                                    </td>
+                            <div class="card-body text-dark">
 
-									<td>
-										<div class="form-group ">
-											    <select id="month-select" name="month" class="form-control" required>
-													<option value="">Select Month</option>
-												</select>
-												<script language="javascript"> print_months("month-select"); </script>
-										</div>
-                                    </td>
-									
-									<td>
-                                    <center>
-										<div class="form-group ">
-											<button type="submit" value="Yield" name="Rainfall_Predict" class="btn btn-success btn-submit">Predict</button>
-										</div>
-                                    
-                                    </center>
-                                    </td>
-                                </tr>
-                            </tbody>
-							
-					
-	</table>
-	</div>
-	</form>
-</div>
+                                <table class="table table-striped table-hover table-bordered text-center display"
+                                    id="myTable">
 
-<div class="card text-white bg-gradient-success mb-3">
-				  <div class="card-header">
-				  <span class=" text-success display-4" > Result  </span>					
-				  </div>
+                                    <thead>
+                                        <tr class="font-weight-bold text-default">
+                                            <th>
+                                                <center>Region</center>
+                                            </th>
+                                            <th>
+                                                <center>Month</center>
+                                            </th>
+                                            <th>
+                                                <center>Prediction</center>
+                                            </th>
 
-					<h4>
-					<?php 
+
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr class="text-center">
+
+                                            <td>
+                                                <div class="form-group ">
+                                                    <select id="region-select" name="region" class="form-control"
+                                                        required>
+                                                        <option value="">Select Region</option>
+                                                    </select>
+                                                    <script language="javascript">
+                                                    print_region("region-select");
+                                                    </script>
+                                                </div>
+                                            </td>
+
+                                            <td>
+                                                <div class="form-group ">
+                                                    <select id="month-select" name="month" class="form-control"
+                                                        required>
+                                                        <option value="">Select Month</option>
+                                                    </select>
+                                                    <script language="javascript">
+                                                    print_months("month-select");
+                                                    </script>
+                                                </div>
+                                            </td>
+
+                                            <td>
+                                                <center>
+                                                    <div class="form-group ">
+                                                        <button type="submit" value="Yield" name="Rainfall_Predict"
+                                                            class="btn btn-success btn-submit">Predict</button>
+                                                    </div>
+
+                                                </center>
+                                            </td>
+                                        </tr>
+                                    </tbody>
+
+
+                                </table>
+                            </div>
+                        </form>
+                    </div>
+
+                    <div class="card text-white bg-gradient-success mb-3">
+                        <div class="card-header">
+                            <span class=" text-success display-4"> Result </span>
+                        </div>
+
+                        <h4>
+                            <?php 
 					
 					
 					if(isset($_POST['Rainfall_Predict'])){
@@ -131,19 +145,19 @@ $query4 = "SELECT * from farmerlogin where email='$user_check'";
 					}
 
                     ?>
-					</h4>
+                        </h4>
+                    </div>
+
+
+
+                </div>
             </div>
- 
-	
-	
-            </div>
-          </div>  
-       </div>
-		 
-</section>
+        </div>
+
+    </section>
 
     <?php require("footer.php");?>
 
 </body>
-</html>
 
+</html>
